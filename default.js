8d357ceddff31198344dfbae4ad928359f1ae859
Derek Miller <derekmiller9@gmail.com>
2016-08-30 11:32:17 -0700

    Search function only searchers strings in array of objects. #1

---
+++ default.js
@@ -0,0 +1,40 @@
+
+var search = document.getElementById('search')
+search.addEventListener('click', function() {
+  var input = document.getElementById('input');
+  var word = input.value;
+  var matches = [];
+
+
+
+  //finds description of item and creates new array matches of matched text
+  for (var i = 0; i < places.length; i++) {
+    for (param in places[i]) {
+      if (typeof places[i][param] === 'string') {
+        if(places[i][param].indexOf(word) !== -1)
+        matches.push(places[i]);
+      }
+    }
+  }
+  console.log(matches);
+
+  for (var i = 0; i < matches.length; i++) {
+    var results = document.getElementById('results');
+    results.appendChild(item(matches[i]));
+  }
+});
+
+function item(data) {
+  var item = document.createElement('div');
+  item.classList.add('panel')
+  item.classList.add('panel-default')
+  var place = document.createElement('h1');
+  var address = document.createElement('h3');
+  // address.classList.add('br')
+  place.textContent = data.name;
+  address.textContent = data.address;
+  item.appendChild(place);
+  item.appendChild(address);
+
+  return item;
+}
