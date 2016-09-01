fb6218968f4dd0cdc386d896468b09f8a9b7d038
Derek Miller <derekmiller9@gmail.com>
2016-09-01 11:29:05 -0700

    Display place when selected in results. #2

--- default.js
+++ default.js
@@ -1,3 +1,69 @@
+function item(data) {
+  var item = document.createElement('div');
+  item.classList.add('row');
+  item.classList.add('panel');
+  item.classList.add('panel-default');
+  var itemLeft = document.createElement('div');
+  itemLeft.classList.add('list-left');
+  var itemRight = document.createElement('div');
+  itemRight.classList.add('list-right')
+  var place = document.createElement('h3');
+  place.setAttribute('data-name', data.name);
+  place.classList.add('hover');
+  place.classList.add('place-name')
+  var address = document.createElement('div');
+  var hours = document.createElement('p');
+  place.textContent = data.name;
+  hours.textContent = 'Open: ' + data.hours;
+  address.textContent = data.address;
+  item.appendChild(itemLeft);
+  item.appendChild(itemRight);
+  itemLeft.appendChild(place);
+  itemLeft.appendChild(hours);
+  itemRight.appendChild(address);
+
+  return item;
+}
+
+function seek(prop) {
+  var select = document.createElement('div');
+  select.classList.add('select');
+  select.classList.add('row');
+  var items = document.createElement('div');
+  items.classList.add('clear');
+  var place = document.createElement('h3');
+  place.textContent = prop.name;
+  var address = document.createElement('div');
+  address.textContent = prop.address;
+  var hours = document.createElement('div');
+  hours.textContent = 'Open: ' + prop.hours;
+  var reservations = document.createElement('div');
+  reservations.textContent = 'Takes Reservations: ' + prop.reservations;
+  var delivery = document.createElement('div');
+  delivery.textContent = 'Delivers: ' + prop.delivery;
+  var credit = document.createElement('div');
+  credit.textContent = 'Takes Credit Card: ' + prop.creditCard;
+  var wifi = document.createElement('div');
+  wifi.textContent = 'Wi-Fi: ' + prop.wifi;
+  var good = document.createElement('div');
+  good.textContent = 'Good For: ' + prop.goodFor;
+  var alcohol = document.createElement('div');
+  alcohol.textContent = 'Alcohol: ' + prop.alcohol;
+  place.classList.add('list-left');
+  address.classList.add('list-right');
+  select.appendChild(place);
+  select.appendChild(address);
+  select.appendChild(items);
+  items.appendChild(hours);
+  items.appendChild(good);
+  items.appendChild(alcohol);
+  items.appendChild(reservations);
+  items.appendChild(delivery);
+  items.appendChild(credit);
+  items.appendChild(wifi);
+
+  return select;
+}
 
 var search = document.getElementById('search')
 search.addEventListener('click', function() {
@@ -25,32 +91,6 @@ search.addEventListener('click', function() {
 
 });
 
-function item(data) {
-  var item = document.createElement('div');
-  item.classList.add('row');
-  item.classList.add('panel');
-  item.classList.add('panel-default');
-  var itemLeft = document.createElement('div');
-  itemLeft.classList.add('list-left');
-  var itemRight = document.createElement('div');
-  itemRight.classList.add('list-right')
-  var place = document.createElement('h3');
-  place.setAttribute('data-name', data.name);
-  place.classList.add('hover');
-  place.classList.add('place-name')
-  var address = document.createElement('div');
-  var hours = document.createElement('p');
-  place.textContent = data.name;
-  hours.textContent = 'Open: ' + data.hours;
-  address.textContent = data.address;
-  item.appendChild(itemLeft);
-  item.appendChild(itemRight);
-  itemLeft.appendChild(place);
-  itemLeft.appendChild(hours);
-  itemRight.appendChild(address);
-
-  return item;
-}
 
 function clear(target) {
   while(target.firstChild) {
@@ -85,37 +125,9 @@ document.body.addEventListener('click', function view(theEvent) {
   }
 });
 
-function seek(prop) {
-  var select = document.createElement('div');
-  var place = document.createElement('h3');
-  place.textContent = prop.name;
-  // var address = document.createElement('div');
-  // var hours = document.createElement('li');
-  // var reservations = document.createElement('li');
-  // var delivery = document.createElement('li');
-  // var credit = document.createElement('li');
-  // var wifi = document.createElement('li');
-  // var good = document.createElement('li');
-  // var alcohol = document.createElement('li');
-  place.classList.add('list-left');
-  // address.classList.add('list-right');
-  select.appendChild(place);
-  // select.appendChild(address);
-
-  return select;
-}
-
-
-
-var back = document.createElement('div');
-back.classList.add('back');
-back.classList.add('hover');
-back.textContent = 'Back to Search Results';
-individual.appendChild(back);
 document.body.addEventListener('click', function rebound(theBack) {
   if (theBack.target.className.indexOf('back') !== -1) {
     results.classList.toggle('hidden');
     individual.classList.toggle('hidden');
-    clear(select);
   }
 });
