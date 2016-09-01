68ddd06af2174b885b5b74a9f21e8f9dae35ef9f
Derek Miller <derekmiller9@gmail.com>
2016-09-01 10:13:39 -0700

    Commit updated example

--- default.js
+++ default.js
@@ -22,6 +22,7 @@ search.addEventListener('click', function() {
     var results = document.getElementById('results');
     results.appendChild(item(matches[i]));
   }
+
 });
 
 function item(data) {
@@ -35,6 +36,7 @@ function item(data) {
   itemRight.classList.add('list-right')
   var place = document.createElement('h3');
   place.setAttribute('data-name', data.name);
+  place.classList.add('hover');
   place.classList.add('place-name')
   var address = document.createElement('div');
   var hours = document.createElement('p');
@@ -56,10 +58,64 @@ function clear(target) {
   }
 }
 
-
 document.body.addEventListener('click', function view(theEvent) {
   if (theEvent.target.className.indexOf('place-name') !== -1) {
+    var results = document.getElementById('results');
+    results.classList.add('hidden');
+    var individual = document.getElementById('individual');
+    individual.classList.toggle('hidden');
     var name = theEvent.target.getAttribute('data-name');
-    console.log(name);
+    var match = [];
+    var individual = document.getElementById('individual');
+
+    for (var i = 0; i < places.length; i++) {
+      for (param in places[i]) {
+        if (typeof places[i][param] === 'string') {
+          if (places[i][param].indexOf(name) === 0) {
+            match.push(places[i]);
+          }
+        }
+      }
+    }
+    console.log(match);
+    for (var i = 0; i < match.length; i++) {
+      var individual = document.getElementById('individual');
+      individual.appendChild(seek(match[i]));
+    }
+  }
+});
+
+function seek(prop) {
+  var select = document.createElement('div');
+  var place = document.createElement('h3');
+  place.textContent = prop.name;
+  // var address = document.createElement('div');
+  // var hours = document.createElement('li');
+  // var reservations = document.createElement('li');
+  // var delivery = document.createElement('li');
+  // var credit = document.createElement('li');
+  // var wifi = document.createElement('li');
+  // var good = document.createElement('li');
+  // var alcohol = document.createElement('li');
+  place.classList.add('list-left');
+  // address.classList.add('list-right');
+  select.appendChild(place);
+  // select.appendChild(address);
+
+  return select;
+}
+
+
+
+var back = document.createElement('div');
+back.classList.add('back');
+back.classList.add('hover');
+back.textContent = 'Back to Search Results';
+individual.appendChild(back);
+document.body.addEventListener('click', function rebound(theBack) {
+  if (theBack.target.className.indexOf('back') !== -1) {
+    results.classList.toggle('hidden');
+    individual.classList.toggle('hidden');
+    clear(individual);
   }
 });
